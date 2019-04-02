38f91739928f5bad90bdb0d4d36faaa5b9fb697b
durikang <kooda21@naver.com>
2019-04-02 16:27:53 +0900

    2번 3번 문제 품

---
+++ Chapter6/2번/ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    
+
+    class Program
+    {
+        public static void Mean(out double mean, params double[] arr)
+        {
+            double sum = 0;
+            for(int i = 0; i < arr.Length; i++)
+                sum+= arr[i];
+            sum /= 5;
+            mean = sum;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] arr = new double[] { 1, 2, 3, 4 };
+            double mean = 0;
+            Mean(out mean, arr);
+            Console.WriteLine(mean);
+
+        }
+    }
+}
---
+++ Chapter6/3번/3번/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3번
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int resultA;
+            Plus(a, b,out resultA);
+            Console.WriteLine($"{a}+{b}={resultA}");
+            double x = 2.4;
+            double y = 3.1;
+            double resultB;
+            Plus(x, y, out resultB);
+            Console.WriteLine($"{x}+{y}={resultB}");
+
+        }
+        public static void Plus(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+        public static void Plus(double a, double b, out double c)
+        {
+            c = a + b;
+        }
+    }
+
+
+
+}
